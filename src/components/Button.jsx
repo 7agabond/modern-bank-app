bed7fca2d297d8d926a11cce7c5517e67039b551
Andres Garza <hanato21@icloud.com>
2023-12-05 17:24:05 -0600

    Testimonials complete

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`transition-all py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-transparent ${styles} hover:text-blue-50 hover:translate-y-1 hover:transition-all`}
+      className={`transition-all py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-transparent ${styles} rounded-[10px] hover:text-blue-50 hover:translate-y-1 hover:transition-all`}
     >
       Get Started
     </button>
