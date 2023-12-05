e8608fb863f88503062df099f0ac3676ff234c63
Andres Garza <hanato21@icloud.com>
2023-12-05 17:42:30 -0600

    Visuals update

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,22 @@
+import styles from "../style";
+import Button from "./Button";
 import React from "react";
 
-const CTA = () => {
-  return <div>CTA</div>;
-};
+const CTA = () => (
+  <section
+    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+  >
+    <div>
+      <h2>Try our service instantly!</h2>
+      <p>
+        Everything you need to accept card payments and grow your business
+        anywhere on the planet.
+      </p>
+    </div>
+    <div>
+      <Button />
+    </div>
+  </section>
+);
 
 export default CTA;
--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,24 @@
+import { clients } from "../constants";
+import styles from "../style";
 import React from "react";
 
-const Clients = () => {
-  return <div>Clients</div>;
-};
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className={`transition-all flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[150px] min-h-[120px] rounded-[10px] hover:bg-white hover:transition-all`}
+        >
+          <img
+            src={client.logo}
+            alt="client"
+            className="sm:w-[192px] w-[100px] object-contain"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
 export default Clients;
